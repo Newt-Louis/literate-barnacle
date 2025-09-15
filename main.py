a9c25e6d6dbeb358571362ae94c966794d1dedf5
Newt_Louis <l.ngoc.17395@gmail.com>
2025-09-16 01:13:00 +0700

    linked pushbutton to stackedwidget page show corresponding page

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 import sys
-from PySide6 import QtCore, QtGui, QtWidgets
-from ui.windows.main_window import Ui_MainWindow
-
-class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-        self.setupUi(self)
+from PySide6 import QtWidgets
+from ui.windows.inherit_business_interface.main_app_window import MainWindow
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
     window.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
---
+++ ui/windows/inherit_business_interface/main_app_window.py
@@ -0,0 +1,42 @@
+# ui/windows/main_app_window.py
+
+from PySide6.QtWidgets import QMainWindow
+# Import class giao diện đã được tạo tự động
+from ui.windows.origin_interface.main_window import Ui_MainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        # Gọi hàm khởi tạo của lớp cha (QMainWindow)
+        super().__init__()
+
+        # Tạo một đối tượng từ class giao diện
+        self.ui = Ui_MainWindow()
+        # Dùng phương thức setupUi để vẽ giao diện lên cửa sổ chính (self)
+        self.ui.setupUi(self)
+
+        # Đặt tiêu đề cho cửa sổ
+        # self.setWindowTitle("Local WebServer Stacks Manager")
+
+        # Gọi phương thức để kết nối các tín hiệu (signals) và hành động (slots)
+        self._connect_signals()
+
+        # Thiết lập trang mặc định khi mở ứng dụng
+        self.ui.dashboard_button.setChecked(True)  # Đánh dấu nút Dashboard là đang được chọn
+        self.ui.main_content_area.setCurrentWidget(self.ui.dashboard_page)
+
+    def _connect_signals(self):
+        """
+        Nơi tập trung kết nối tất cả các tín hiệu từ widgets.
+        """
+        # Kết nối các nút bấm ở sidebar với phương thức chuyển trang
+        self.ui.dashboard_button.clicked.connect(
+            lambda: self.ui.main_content_area.setCurrentWidget(self.ui.dashboard_page))
+        self.ui.languages_button.clicked.connect(
+            lambda: self.ui.main_content_area.setCurrentWidget(self.ui.languages_page))
+        self.ui.databases_button.clicked.connect(
+            lambda: self.ui.main_content_area.setCurrentWidget(self.ui.databases_page))
+        self.ui.webserver_button.clicked.connect(
+            lambda: self.ui.main_content_area.setCurrentWidget(self.ui.webserver_page))
+        self.ui.tools_button.clicked.connect(lambda: self.ui.main_content_area.setCurrentWidget(self.ui.tools_page))
+        self.ui.network_button.clicked.connect(lambda: self.ui.main_content_area.setCurrentWidget(self.ui.network_page))
