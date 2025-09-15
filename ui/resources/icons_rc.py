a2133080aa0a95dba84b7743c6b400c2778725e2
Newt_Louis <l.ngoc.17395@gmail.com>
2025-09-15 17:59:58 +0700

    design static ui + create resources icons

---
+++ ui/resources/icons_rc.py
@@ -0,0 +1,73 @@
+# Resource object code (Python 3)
+# Created by: object code
+# Created by: The Resource Compiler for Qt version 6.9.2
+# WARNING! All changes made in this file will be lost!
+
+from PySide6 import QtCore
+
+qt_resource_data = b"\
+\x00\x00\x01\xfb\
+<\
+svg class=\x22svg-i\
+con\x22 style=\x22widt\
+h: 1em; height: \
+1em;vertical-ali\
+gn: middle;fill:\
+ currentColor;ov\
+erflow: hidden;\x22\
+ viewBox=\x220 0 10\
+24 1024\x22 version\
+=\x221.1\x22 xmlns=\x22ht\
+tp://www.w3.org/\
+2000/svg\x22><path \
+d=\x22M128 341.3333\
+33h768a42.666667\
+ 42.666667 0 0 0\
+ 0-85.333333H128\
+a42.666667 42.66\
+6667 0 0 0 0 85.\
+333333z m768 341\
+.333334H128a42.6\
+66667 42.666667 \
+0 0 0 0 85.33333\
+3h768a42.666667 \
+42.666667 0 0 0 \
+0-85.333333z m0-\
+213.333334H128a4\
+2.666667 42.6666\
+67 0 0 0 0 85.33\
+3334h768a42.6666\
+67 42.666667 0 0\
+ 0 0-85.333334z\x22\
+  /></svg>\
+"
+
+qt_resource_name = b"\
+\x00\x05\
+\x00o\xa6S\
+\x00i\
+\x00c\x00o\x00n\x00s\
+\x00\x0a\
+\x06\x11K\xe7\
+\x00b\
+\x00a\x00r\x00s\x00-\x001\x00.\x00s\x00v\x00g\
+"
+
+qt_resource_struct = b"\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x02\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x03\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x10\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
+\x00\x00\x01\x99L\xe95|\
+"
+
+def qInitResources():
+    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+qInitResources()
