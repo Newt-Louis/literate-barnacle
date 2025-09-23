3a4a4ed031cd5e64dd878d8dfc96ee57a3e7d075
Newt_Louis <l.ngoc.17395@gmail.com>
2025-09-23 23:52:56 +0700

    add new Languages page

--- ui/windows/inherit_business_interface/Pages/Languages/LanguagesPageController.py
+++ ui/windows/inherit_business_interface/Pages/Languages/LanguagesPageController.py
@@ -1,15 +1,15 @@
 from PySide6.QtWidgets import QWidget
 # 1. Import lớp giao diện đã được build từ file .py
-from ui.windows.origin_interface import Ui_DashboardPage
+from ui.windows.origin_interface import Ui_LanguagesPage
 
 
-class DashboardPageController(QWidget):
+class DatabasesPageController(QWidget):
     def __init__(self, model):
         super().__init__()
         self.model = model
 
         # 2. Khởi tạo một đối tượng từ lớp giao diện
-        self.ui = Ui_DashboardPage()
+        self.ui = Ui_LanguagesPage()
 
         # 3. Dùng phương thức setupUi() để vẽ giao diện lên chính widget controller này (self)
         self.ui.setupUi(self)
@@ -17,14 +17,14 @@ class DashboardPageController(QWidget):
         # 4. Từ đây, mọi thứ giống hệt như trước!
         # Kết nối signals và slots như bình thường.
         # IDE của bạn bây giờ sẽ gợi ý code khi bạn gõ self.ui. ...
-        self.ui.startall_button.clicked.connect(self.on_save_changes)
-        # self.ui.mysql_radio.toggled.connect(self.on_mysql_selected)
+        self.ui.language_save_change_buttonbox.clicked.connect(self.on_save_changes)
+        self.ui.php_radio.toggled.connect(self.on_mysql_selected)
 
     def on_save_changes(self):
-        print("Lưu cấu hình databases...")
+        print("Lưu cấu hình languages...")
         # Lấy dữ liệu từ self.ui.mysql_version_combobox.currentText()
         # và gọi self.model.save_config(...)
 
     def on_mysql_selected(self, checked):
         if checked:
-            print("MySQL được chọn.")
+            print("PHP được chọn.")
---
+++ ui/windows/inherit_business_interface/Pages/Languages/LanguagesServiceController.py
@@ -0,0 +1,18 @@
+class LanguagesServiceController:
+    def __init__(self, database_model):
+        self.database_model = database_model
+
+    def _on_save_changes(self):
+        print("Languages Page Controller _on_save_changes")
+
+    def _on_cancel_changes(self):
+        print("Languages Page Controller _on_cancel_changes")
+
+    def _on_add_new_data(self):
+        print("Languages Page Controller _on_add_new_data")
+
+    def _on_update_data(self):
+        print("Languages Page Controller _on_update_data")
+
+    def _on_load_data(self):
+        print("Languages Page Controller _on_load_data")
