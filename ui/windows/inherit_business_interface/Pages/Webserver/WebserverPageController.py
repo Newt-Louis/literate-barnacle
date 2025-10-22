09edcdb3d420a6d80347cab2dd2526e0f0a457b9
Newt_Louis <l.ngoc.17395@gmail.com>
2025-10-22 23:47:03 +0700

    set event click + open FileDialog for browse button, set get path for lineEdit

--- ui/windows/inherit_business_interface/Pages/Webserver/WebserverPageController.py
+++ ui/windows/inherit_business_interface/Pages/Webserver/WebserverPageController.py
@@ -1,22 +1,37 @@
-from PySide6.QtWidgets import QWidget, QRadioButton
+from PySide6.QtWidgets import QWidget, QRadioButton, QFileDialog
 from ui.windows.origin_interface import Ui_WebserverPage
 
 
 class WebserverPageController(QWidget):
+    BROWSE_MAP = {
+        "apache_excutable_browseButton": ("file","apache_excutable_lineEdit","(*.exe)"),
+        "apache_sites_enabled_browseButton": ("folder","apache_sites_enabled_lineEdit",""),
+        "apache_alp_browseButton": ("folder","apache_alp_lineEdit",""),
+        "apache_elp_browseButton": ("folder","apache_elp_lineEdit",""),
+        "nginx_excutable_browseButton": ("file", "nginx_excutable_lineEdit", "(*.exe)"),
+        "nginx_sites_enabled_browseButton": ("folder", "nginx_sites_enabled_lineEdit", ""),
+        "nginx_alp_browseButton": ("folder", "nginx_alp_lineEdit", ""),
+        "nginx_elp_browseButton": ("folder", "nginx_elp_lineEdit", ""),
+    }
     def __init__(self, model):
         super().__init__()
         self.model = model
-
         self.ui = Ui_WebserverPage()
-
         self.ui.setupUi(self)
 
-        self.ui.apache_radio.setChecked(True)
+        # Gắn sự kiện
         self.ui.apache_radio.clicked.connect(self.on_webserver_changed)
         self.ui.nginx_radio.clicked.connect(self.on_webserver_changed)
         self.ui.webserver_save_change_buttonbox.clicked.connect(self.on_save_changes)
+        for button in self.findChildren(QWidget):
+            name = button.objectName()
+            #Gắn sự kiện cho tất cả nút Browse
+            if name.endswith("_browseButton"):
+                button.clicked.connect(self.on_browse_button_clicked)
 
-        # Gọi cập nhật thay đổi cho lần đầu tiên
+        # Set mặc định chọn radio apache đầu tiên
+        self.ui.apache_radio.setChecked(True)
+        # Gọi cập nhật thay đổi disabled cho lần đầu tiên
         self.on_webserver_changed()
 
     def on_save_changes(self):
@@ -36,13 +51,31 @@ class WebserverPageController(QWidget):
 
     def on_webserver_changed(self):
         if self.ui.apache_radio.isChecked():
-            print("Apache được chọn 2")
             self.set_group_enabled("apache_", True)
             self.set_group_enabled("nginx_", False)
         elif self.ui.nginx_radio.isChecked():
-            print("Nginx được chọn 2")
             self.set_group_enabled("apache_", False)
             self.set_group_enabled("nginx_", True)
+
+    def on_browse_button_clicked(self):
+        """Xử lý khi click bất kỳ nút Browse nào"""
+        sender = self.sender()
+        name = sender.objectName()
+        print(f"Clicked: {name}")
+        if name not in self.__class__.BROWSE_MAP:
+            return
+
+        browse_type, lineedit_name, file_filter = self.__class__.BROWSE_MAP[name]
+        line_edit = getattr(self.ui, lineedit_name)
+
+        if browse_type == "file":
+            file_path, _ = QFileDialog.getOpenFileName(self, "Chọn file", "", file_filter)
+            if file_path:
+                line_edit.setText(file_path)
+        else:  # folder
+            folder = QFileDialog.getExistingDirectory(self, "Chọn thư mục", "")
+            if folder:
+                line_edit.setText(folder)
 
 
     # Các widget hiện đang có trong WebserverPage
