0a92ab6408ef9a34cf65c30c629edff95276509e
Newt_Louis <l.ngoc.17395@gmail.com>
2025-10-31 17:41:32 +0700

    create helper function for sessionmanagement

--- core/manager/DashboardManagement.py
+++ core/manager/DashboardManagement.py
@@ -5,7 +5,24 @@ class DashboardManagement:
             cls._instance = super(DashboardManagement, cls).__new__(cls)
             cls._instance._data = {}
         return cls._instance
+
     def set(self,key,value):
-        self._instance._data[key] = value
-    def get(self,key,value):
-        return self._instance._data.get(key,value)
+        self._data[key] = value
+
+    def get(self,key, default=None):
+        return self._data.get(key,default)
+
+    def add_to_current_key(self,top_level_key, key_to_add, value_to_add):
+        if top_level_key in self._data:
+            target_dict = self._data[top_level_key]
+            if isinstance(target_dict, dict):
+                target_dict[key_to_add] = value_to_add
+                return True
+        return False
+
+    def delete(self, key):
+        if key in self._data:
+            del self._data[key]
+
+    def clear(self):
+        self._data.clear()
