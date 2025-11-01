17c3e8f55118b49b39bee8f4820addb130c7585e
Newt_Louis <l.ngoc.17395@gmail.com>
2025-11-01 23:43:56 +0700

    add table projects for auto create virtual host

---
+++ core/database/migrate/2025_11_01_create_table_projects.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+def up(cursor: sqlite3.Cursor):
+    print("Applying migration: create_projects_table...")
+
+    cursor.execute("""
+                   CREATE TABLE IF NOT EXISTS projects
+                   (
+                       id INTEGER PRIMARY KEY AUTOINCREMENT,
+                       project_name TEXT,
+                       language TEXT,
+                       project_path TEXT,
+                       domain TEXT,
+                       is_enabled BOOLEAN
+                   );
+                   """)
+
+# Hàm down để hoàn tác
+def down(cursor: sqlite3.Cursor):
+    print("Reverting migration: create_language_versions_table...")
+    cursor.execute("DROP TABLE IF EXISTS language_versions;")
